c2157d90c05dd6583d2ad86715e7d97a085df9ee
mac <mbasharat596@gmail.com>
2026-07-15 23:40:22 +0500

    login & dashbaord mastering update

--- app/Http/helpers/helpers.php
+++ app/Http/helpers/helpers.php
@@ -1,12 +1,11 @@
 <?php
 
-if(!function_exists('setSidebarActive')) {
+if (!function_exists('setSidebarActive')) {
     /**
      * Set the sidebar menu item as active.
      */
-    function setSidebarActive($arrays, $routes ):?string
+    function setSidebarActive(array $routes): ?string
     {
-
         if (request()->routeIs($routes)) {
             return 'active';
         }
